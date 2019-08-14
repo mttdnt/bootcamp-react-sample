38697c628ea4a3677fe52a8b2e9d68412c94ebf2
Shadda Mohammed <shaddamohammed1@gmail.com>
2019-08-14 17:47:01 -0400

    Adding Clock

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Video from "./Routes/Video";
 import Home from "./Routes/Home";
+import Clock from "./Components/Clock";
+
 import "./App.css";
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Use Youtube API to Search Videos</h1>
+        <Clock />
         <Router>
           <Route exact path="/(|home)/" component={Home} />
           <Route exact path="/video/:videoId" component={Video} />
